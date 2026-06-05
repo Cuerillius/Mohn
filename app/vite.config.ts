e24d1a5bafa23b12eb85ae8b72f24de0ec1f3a1f
cyrillweber <cyrill.weber1@edu.tbz.ch>
2026-06-05 13:33:03 +0200

    customize installer

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
@@ -20,6 +19,5 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
     watch: { ignored: ["**/src-tauri/**"] },
-    allowedHosts: ["stream.cyri.li"],
   },
 }));
